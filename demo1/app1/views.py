de206037f12b4b4ca90df258501e94d1f3318a64
surya-rj <surya.j@kce.ac.in>
2023-08-09 20:37:35 +0530

    first change

---
+++ demo1/app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from . import urls
+# Create your views here.
+def index(request):
+    t=loader.get_template('index.html')
+    return HttpResponse(t.render())
